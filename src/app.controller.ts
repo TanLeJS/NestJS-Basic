867be038f66b40dbbccbf76b0b797f49a3f09cc8
TanLeJS <tanlengoc2000@gmail.com>
2024-02-13 20:02:59 -0500

    video 20

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Render } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ConfigService } from '@nestjs/config';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
+  constructor(
+    private readonly appService: AppService,
+    private configService: ConfigService,
+    ) {}
   @Get()
+  @Render("home")
+  handleHomepage(){
+    console.log(">>> Check port = " , this.configService.get<string>("PORT"))
+  }
   getHello(): string {
     return this.appService.getHello();
   }
