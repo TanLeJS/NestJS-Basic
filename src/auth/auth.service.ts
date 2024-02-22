a6d9b6195eeabf03e674fa7ccf11e0e7a8d9413e
TanLeJS <tanlengoc2000@gmail.com>
2024-02-22 14:23:02 -0500

    video 66

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
+import ms from 'ms';
 import { RegisterUserDto } from 'src/users/dto/register-user.dto';
 import { IUser } from 'src/users/users.interface';
 import { UsersService } from 'src/users/users.service';
@@ -8,7 +10,8 @@ import { UsersService } from 'src/users/users.service';
 export class AuthService  {
     constructor(
         private usersService: UsersService,
-        private jwtService: JwtService
+        private jwtService: JwtService,
+        private configService: ConfigService,
         ) {}
     //username/password là 2 tham số thư viện passport trả về
     async validateUser(username: string, pass: string): Promise<any> {
@@ -32,12 +35,16 @@ export class AuthService  {
         email,
         role
         };
+        const refresh_token = this.createRefreshToken(payload)
         return {
         access_token: this.jwtService.sign(payload),
-        _id,
-        name,
-        email,
-        role
+        refresh_token,
+        users: {
+            _id,
+            name,
+            email,
+            role
+        },
         };
 }
     async register(user: RegisterUserDto){
@@ -46,5 +53,13 @@ export class AuthService  {
             _id: newUser?._id,
             createdAt: newUser?.createdAt
         }
+    }
+
+    createRefreshToken = (payload: any) => {
+        const refresh_token =  this.jwtService.sign(payload, {
+            secret: this.configService.get<string>("JWT_REFRESH_TOKEN_SECRET"),
+            expiresIn: ms(this.configService.get<string>('JWT_REFRESH_EXPIRE')) / 1000,
+        })
+        return refresh_token
     }
 }
--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,6 +1,10 @@
-import { OmitType, PartialType } from '@nestjs/mapped-types';
+import { OmitType } from '@nestjs/mapped-types';
+import { IsNotEmpty } from 'class-validator';
 import { CreateUserDto } from './create-user.dto';
 
-export class UpdateUserDto extends OmitType(CreateUserDto, ['password'] as const) {
-    _id: string;
+export class UpdateUserDto extends OmitType(CreateUserDto, [
+  'password'
+] as const) {
+  @IsNotEmpty({ message: 'Id không được để trống' })
+  _id: string;
 }
